c06859fd671d30112f60a50f93e8f405a3b112e3
idcetim <javirofe96@gmail.com>
2023-05-01 15:12:43 +0200

    Añadir outline

---
+++ src/Components/Outline.jsx
@@ -0,0 +1,16 @@
+import '../styles/Outline.css'
+const Outline = () => {
+  return (
+    <>
+      <h2> Propiedad outline</h2>
+      <div className="outline-non">
+        <span> Texto </span>
+      </div>
+      <div className="outline-yes">
+        <span> Texto </span>
+      </div>
+    </>
+  )
+}
+
+export default Outline
--- src/routes/Basico2.jsx
+++ src/routes/Basico2.jsx
@@ -1,5 +1,10 @@
+import Outline from "../Components/Outline"
 const Basico2 = () => {
-
+return(
+  <>
+  <Outline />
+  </>
+)
 }
 
 export default Basico2
