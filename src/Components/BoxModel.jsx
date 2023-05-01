bd6f9fed05662935767bd659d17c496408d247bc
idcetim <javirofe96@gmail.com>
2023-05-01 10:05:48 +0200

    Añadir Padding

--- src/Components/BoxModel.jsx
+++ src/Components/BoxModel.jsx
@@ -4,8 +4,8 @@ const BoxModel = () => {
     <div className='box'>
       <div className='small-box'>
         <h1>Ejemplo de Box model</h1>
-        <span>Esto es un texto</span></div>
-
+        <span>Esto es un texto</span>
+      </div>
     </div>)
 }
 
