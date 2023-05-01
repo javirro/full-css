c88116a5e155df96a83a589c546af83f8184fc9e
idcetim <javirofe96@gmail.com>
2023-05-01 10:22:50 +0200

    Añadir Box-sizing
    Existen dos opciones: content-box y border-box

--- src/Components/BoxModel.jsx
+++ src/Components/BoxModel.jsx
@@ -2,8 +2,14 @@ import '../styles/BoxModel.css'
 const BoxModel = () => {
   return (
     <div className='box'>
+      <h1>400 px de Width solo para el contenido</h1>
       <div className='small-box'>
-        <h1>Ejemplo de Box model</h1>
+        <h1>Ejemplo de Box model: content-box</h1>
+        <span>Esto es un texto</span>
+      </div>
+      <h1>400 px de Width solo para todo el bloque</h1>
+      <div className='small-box-2'>
+        <h1>Ejemplo de Box model: : border-box</h1>
         <span>Esto es un texto</span>
       </div>
     </div>)
